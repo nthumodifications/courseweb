6a48fb8d352c78f6474c44ba2deabc912e246841
Joshimello <joshualeanjw@gmail.com>
2023-09-23 14:21:26 +0800

    nav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,5 +1,4 @@
-import { faClock, faCalendar, faCalendarPlus, faMap } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as I from 'react-feather';
 import Link from "next/link";
 import { FC } from "react";
 
@@ -8,34 +7,37 @@ const SideNav:FC = () => {
         {
             title: 'Today',
             href: '/today',
-            icon: <FontAwesomeIcon icon={faClock} />
+            icon: <I.Clock strokeWidth="1"/>
         },
         {
             title: 'Timetable',
             href: '/timetable',
-            icon: <FontAwesomeIcon icon={faCalendar}/>
+            icon: <I.Calendar strokeWidth="1"/>
         },
         {
             title: 'Courses',
             href: '/courses',
-            icon: <FontAwesomeIcon icon={faCalendarPlus}/>
+            icon: <I.BookOpen strokeWidth="1"/>
         },
         {
             title: 'Locations',
             href: '/locations',
-            icon: <FontAwesomeIcon icon={faMap}/>
+            icon: <I.Map strokeWidth="1"/>
         }
     ]
     return (
-        <nav className="h-screen px-8 py-4 bg-white">
+        <nav className="h-screen flex flex-col w-max gap-4 p-6">
             {links.map((link, index) => (
-                <Link 
-                    key={index} 
-                    href={link.href} 
-                    className="flex items-center space-x-2 px-4 py-2 text-base text-gray-700 hover:text-violet-700">
-                        <span className="w-6 h-6">{link.icon}</span>
-                        
-                        <span className="text-semibold">{link.title}</span>
+                <Link className="flex items-center gap-4 hover:text-fuchsia-600 hover:underline"
+                    key={index} href={link.href}>
+
+                    <span className="w-6">
+                        {link.icon}
+                    </span>
+                    <span className="text-semibold">
+                        {link.title}
+                    </span>
+
                 </Link>
             ))}
         </nav>
