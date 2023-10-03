757b62fd308d80e3800acbc8f82c3a754944776c
ImJustChew <chewtzihwee@gmail.com>
2023-10-03 18:57:47 +0800

    Added Loading Bar and colored title

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -5,7 +5,7 @@ import supabase, { CourseDefinition } from "@/config/supabase";
 import { departments } from "@/const/departments";
 import { useSettings } from "@/hooks/contexts/settings";
 import { Database } from "@/types/supabase";
-import { Autocomplete, AutocompleteOption, Button, Checkbox, Chip, FormControl, FormHelperText, FormLabel, Input, List, ListItem, ListItemContent, ListItemDecorator, Sheet, Typography } from "@mui/joy";
+import { Autocomplete, AutocompleteOption, Button, Checkbox, Chip, FormControl, FormHelperText, FormLabel, Input, LinearProgress, List, ListItem, ListItemContent, ListItemDecorator, Sheet, Typography } from "@mui/joy";
 import { NextPage } from "next";
 
 import { useEffect, useState, FC } from "react";
@@ -18,11 +18,11 @@ const CourseListItem: FC<{ course: CourseDefinition }> = ({ course }) => {
     return <div className="text-gray-600 px-4">
         <div className="flex flex-row justify-between">
             <div className="mb-3">
-                <h1 className="font-semibold text-lg">{course.department} {course.course}-{course.class} {course.name_zh} - {course.raw_teacher_zh}</h1>
+                <h1 className="font-semibold text-lg text-fuchsia-800">{course.department} {course.course}-{course.class} {course.name_zh} - {course.raw_teacher_zh}</h1>
                 <h3 className="text-base text-gray-800 mt-0">{course.name_en} - {course.raw_teacher_en}</h3>
             </div>
             <div className="flex flex-row space-x-1 mb-2">
-                <Users/>
+                <Users />
                 <span className="">{course.capacity} / {course.reserve}R</span>
             </div>
         </div>
@@ -43,13 +43,13 @@ const CourseListItem: FC<{ course: CourseDefinition }> = ({ course }) => {
                     disabled={false}
                     size="md"
                     variant="outlined"
-                >English</Chip>:
-                <Chip
-                    color="success"
-                    disabled={false}
-                    size="md"
-                    variant="outlined"
-                >國語</Chip>}
+                >English</Chip> :
+                    <Chip
+                        color="success"
+                        disabled={false}
+                        size="md"
+                        variant="outlined"
+                    >國語</Chip>}
             </div>
         </div>
     </div>
@@ -67,8 +67,9 @@ const CoursePage: NextPage = () => {
     // const { language } = useSettings();
 
     const [courses, setCourses] = useState<CourseDefinition[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
     const [totalCount, setTotalCount] = useState<number>(0);
-    const { control, watch,  } = useForm<FormTypes>({
+    const { control, watch, } = useForm<FormTypes>({
         defaultValues: {
             textSearch: '',
             level: [],
@@ -79,23 +80,30 @@ const CoursePage: NextPage = () => {
     })
 
     //codes should be 4 char, so we need to pad it at the end
-    const padEnd = (str: string) =>  str + ' '.repeat(4 - str.length)
+    const padEnd = (str: string) => str + ' '.repeat(4 - str.length)
 
     const searchQueryFunc = useDebouncedCallback((filters: FormTypes) => {
         console.log(filters);
         (async () => {
-            let temp = supabase
-            .from('courses')
-            .select('*', { count: 'exact' })
-            if(filters.textSearch) temp = temp.textSearch('name_en', `'${filters.textSearch.split(' ').join("' & '")}'`)
-            if(filters.level.length) temp = temp.or(filters.level.map(level => `and(course.gte.${level}000,course.lte.${level}999)`).join(','))
-            if(filters.language.length) temp = temp.or(filters.language.map(lang => `language.eq.${lang}`).join(','))
-            if(filters.department.length) temp = temp.in('department', filters.department.map(({code}) => padEnd(code)))
-            let { data: courses, error, count } = await temp.limit(300)
-            console.log('filter courses',courses)
-            setTotalCount(count ?? 0)
-            if(error) console.log(error);
-            else setCourses(courses!);
+            setLoading(true);
+            try {
+                let temp = supabase
+                    .from('courses')
+                    .select('*', { count: 'exact' })
+                if (filters.textSearch) temp = temp.textSearch('name_en', `'${filters.textSearch.split(' ').join("' & '")}'`)
+                if (filters.level.length) temp = temp.or(filters.level.map(level => `and(course.gte.${level}000,course.lte.${level}999)`).join(','))
+                if (filters.language.length) temp = temp.or(filters.language.map(lang => `language.eq.${lang}`).join(','))
+                if (filters.department.length) temp = temp.in('department', filters.department.map(({ code }) => padEnd(code)))
+                let { data: courses, error, count } = await temp.limit(300)
+                console.log('filter courses', courses)
+                setTotalCount(count ?? 0)
+                if (error) console.log(error);
+                else setCourses(courses!);
+            }
+            catch(e) {
+                console.error(e);
+            }
+            setLoading(false);
         })()
     }, 1000);
 
@@ -120,20 +128,20 @@ const CoursePage: NextPage = () => {
                     <h6 className="text-gray-600">Courses</h6>
                     <h6 className="text-gray-600">Found: {totalCount} Courses</h6>
                 </div>
-                
+                {loading && <LinearProgress/>}
                 {courses.map((course, index) => (
-                    <CourseListItem key={index} course={course}/>
+                    <CourseListItem key={index} course={course} />
                 ))}
             </div>
             <Sheet variant="outlined" sx={{ p: 2, borderRadius: 'sm', width: 300 }}>
                 <Typography
                     id="filter-status"
                     sx={{
-                    textTransform: 'uppercase',
-                    fontSize: 'xs',
-                    letterSpacing: 'lg',
-                    fontWeight: 'lg',
-                    color: 'text.secondary',
+                        textTransform: 'uppercase',
+                        fontSize: 'xs',
+                        letterSpacing: 'lg',
+                        fontWeight: 'lg',
+                        color: 'text.secondary',
                     }}
                 >
                     Refine
@@ -141,9 +149,9 @@ const CoursePage: NextPage = () => {
                 <div role="group" aria-labelledby="filter-status">
                     <List>
                         <ListItem variant="plain" sx={{ borderRadius: 'sm' }}>
-                            <MultiSelectControl 
-                                control={control} 
-                                name="level" 
+                            <MultiSelectControl
+                                control={control}
+                                name="level"
                                 options={[
                                     { value: 1, label: '1xxx' },
                                     { value: 2, label: '2xxx' },
@@ -159,9 +167,9 @@ const CoursePage: NextPage = () => {
                             />
                         </ListItem>
                         <ListItem variant="plain" sx={{ borderRadius: 'sm' }}>
-                            <MultiSelectControl 
-                                control={control} 
-                                name="language" 
+                            <MultiSelectControl
+                                control={control}
+                                name="language"
                                 options={[
                                     { value: '英', label: 'English' },
                                     { value: '中', label: '國語' },
@@ -175,46 +183,46 @@ const CoursePage: NextPage = () => {
                                 <Controller
                                     control={control}
                                     name="department"
-                                    render={({ field: {value, onChange} }) => (
-                                    <Autocomplete
-                                        placeholder="Departments"
-                                        value={value}
-                                        onChange={(e, v) => onChange(v)}
-                                        multiple={true}
-                                        getOptionLabel={(option) => `${option.code} ${option.name_zh}`}
-                                        isOptionEqualToValue={(option, value) => option.code === value.code}
-                                        renderTags={(value, getTagProps) => 
-                                            value.map((option, index) => 
-                                                <Chip 
-                                                    variant="soft"
-                                                    {...getTagProps({ index })}
-                                                >
-                                                    {`${option.code}`} 
-                                                </Chip>
-                                            )
-                                        }
-                                        renderOption={(props, option) => (
-                                            <AutocompleteOption {...props}>
-                                                <ListItemDecorator>
-                                                    <span className="text-sm font-semibold">{option.code}</span>
-                                                </ListItemDecorator>
-                                                <ListItemContent sx={{ fontSize: 'sm' }}>
-                                                    <Typography level="body-xs">
-                                                    {option.name_zh} {option.name_en}
-                                                    </Typography>
-                                                </ListItemContent>
-                                            </AutocompleteOption>
-                                        )}
-                                        options={departments}
-                                        sx={{ width: 250 }}
-                                    />
-                                )}/>
+                                    render={({ field: { value, onChange } }) => (
+                                        <Autocomplete
+                                            placeholder="Departments"
+                                            value={value}
+                                            onChange={(e, v) => onChange(v)}
+                                            multiple={true}
+                                            getOptionLabel={(option) => `${option.code} ${option.name_zh}`}
+                                            isOptionEqualToValue={(option, value) => option.code === value.code}
+                                            renderTags={(value, getTagProps) =>
+                                                value.map((option, index) =>
+                                                    <Chip
+                                                        variant="soft"
+                                                        {...getTagProps({ index })}
+                                                    >
+                                                        {`${option.code}`}
+                                                    </Chip>
+                                                )
+                                            }
+                                            renderOption={(props, option) => (
+                                                <AutocompleteOption {...props}>
+                                                    <ListItemDecorator>
+                                                        <span className="text-sm font-semibold">{option.code}</span>
+                                                    </ListItemDecorator>
+                                                    <ListItemContent sx={{ fontSize: 'sm' }}>
+                                                        <Typography level="body-xs">
+                                                            {option.name_zh} {option.name_en}
+                                                        </Typography>
+                                                    </ListItemContent>
+                                                </AutocompleteOption>
+                                            )}
+                                            options={departments}
+                                            sx={{ width: 250 }}
+                                        />
+                                    )} />
                             </FormControl>
                         </ListItem>
                         <ListItem variant="plain" sx={{ borderRadius: 'sm' }}>
-                            <MultiSelectControl 
-                                control={control} 
-                                name="others" 
+                            <MultiSelectControl
+                                control={control}
+                                name="others"
                                 options={[
                                     { value: 'xclass', label: 'X-Class' },
                                 ]}
@@ -227,7 +235,7 @@ const CoursePage: NextPage = () => {
                     variant="outlined"
                     color="neutral"
                     size="sm"
-                    onClick={() => {}
+                    onClick={() => { }
                     }
                     sx={{ px: 1.5, mt: 1 }}
                 >
@@ -235,7 +243,7 @@ const CoursePage: NextPage = () => {
                 </Button>
             </Sheet>
         </div>
-        
+
     )
 }
 
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,8 +1,11 @@
+'use client';
 import * as I from 'react-feather';
 import Link from "next/link";
 import { FC } from "react";
+import { usePathname } from 'next/navigation';
 
 const SideNav:FC = () => {
+    const pathname = usePathname();
     const links = [
         {
             title: 'Today',
@@ -25,10 +28,11 @@ const SideNav:FC = () => {
             icon: <I.Map strokeWidth="1"/>
         }
     ]
+    console.log(pathname)
     return (
         <nav className="h-screen flex flex-col w-max gap-4 p-6">
             {links.map((link, index) => (
-                <Link className="flex items-center gap-4 hover:text-fuchsia-600 hover:underline"
+                <Link className={`flex items-center gap-4 hover:text-fuchsia-600 hover:underline transition-colors ${link.href == pathname ? "text-fuchsia-600":"text-gray-600"}`}
                     key={index} href={link.href}>
 
                     <span className="w-6">
