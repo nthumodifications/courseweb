1e1615c1a9b76563c0c1d3a01e818cc2f543de8f
ImJustChew <chewtzihwee@gmail.com>
2023-10-06 01:35:22 +0800

    Expect null values from API

--- src/components/Today/TodaySchedule.tsx
+++ src/components/Today/TodaySchedule.tsx
@@ -27,16 +27,19 @@ const WeatherIcon: FC<{ date: Date, weather: [
         }[]
     }
 ]}> = ({ date, weather }) => {
+    console.log(weather)
     if(!weather) return <></>;
 
     const weatherData = weather[0].time.find(t => new Date(t.startTime) <= date && new Date(t.endTime) >= date);
     const weatherDescription = weather[1].time.find(t => new Date(t.startTime) <= date && new Date(t.endTime) >= date);
 
+    if(!weatherData || !weatherDescription) return <></>;
+
     return (
-        <Tooltip title={weatherDescription?.elementValue[0].value} placement="bottom" sx={{maxWidth: '180px'}}>
+        <Tooltip title={weatherDescription?.elementValue?.[0].value} placement="bottom" sx={{maxWidth: '180px'}}>
             <img 
                 className="w-12 h-12"
-                src={`https://www.cwa.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/${weatherData!.elementValue[1].value}.svg`} 
+                src={`https://www.cwa.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/${weatherData!.elementValue?.[1].value}.svg`} 
             />
         </Tooltip>    
     )
