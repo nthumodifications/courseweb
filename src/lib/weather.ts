2fa58f1ae4bd61252868f0830e10bc8a3a2d3473
ImJustChew <chewtzihwee@gmail.com>
2023-10-15 13:23:06 +0800

    fixed weather

--- src/lib/weather.ts
+++ src/lib/weather.ts
@@ -1,7 +1,11 @@
 'use server';
 
 export async function getWeatherData() {
-    const res = await fetch(`https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-055?Authorization=${process.env.CWA_API_KEY}&format=JSON&locationName=%E6%9D%B1%E5%8D%80&elementName=Wx,WeatherDescription` ,{ cache: 'force-cache'})
+    const res = await fetch(
+      `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-055?Authorization=${process.env.CWA_API_KEY}&format=JSON&locationName=%E6%9D%B1%E5%8D%80&elementName=Wx,WeatherDescription` ,
+      { 
+        next: { revalidate: 60 * 60 * 24 }
+      })
 
     if (!res.ok) {
       throw new Error('Failed to fetch data')
