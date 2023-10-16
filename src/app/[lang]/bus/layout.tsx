74caece0b0a7b879e484f9a82458ba49c440b457
ImJustChew <chewtzihwee@gmail.com>
2023-10-17 01:18:51 +0800

    Added SSR Title

---
+++ src/app/[lang]/bus/layout.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from 'react'
+
+export const metadata = {
+    title: '校園公車 Bus Schedule | NTHUMods'
+}
+
+export default function ClientLayout({ children }: PropsWithChildren<{}>) {
+    return children
+}
