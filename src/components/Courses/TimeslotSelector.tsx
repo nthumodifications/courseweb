0a9412b8e08f26070986ddcd2006d669f46eef3d
ImJustChew <chewtzihwee@gmail.com>
2023-10-25 01:14:37 +0800

    Added easy selection of rows and cols

--- src/components/Courses/TimeslotSelector.tsx
+++ src/components/Courses/TimeslotSelector.tsx
@@ -14,14 +14,44 @@ const TimeslotSelector: FC<{ value: string[], onChange: (newvalue: string[]) => 
         } else {
             onChange([...value, timecode])
         }
-
     }
+
+    const handleSelectRow = (time: string) => () => {
+        //check if anything is selected in this row
+        const rowSelected = days.some(day => isSelected(day+time));
+        if(rowSelected) {
+            onChange(value.filter(tc => !days.some(day => tc == day+time)))
+        } else {
+            onChange([...value, ...days.map(day => day+time)])
+        }
+    }
+
+    const handleSelectColumn = (day: string) => () => {
+        //check if anything is selected in this column
+        const columnSelected = scheduleTimeSlots.some(timeSlot => isSelected(day+timeSlot.time));
+        if(columnSelected) {
+            onChange(value.filter(tc => !scheduleTimeSlots.some(timeSlot => tc == day+timeSlot.time)))
+        } else {
+            onChange([...value, ...scheduleTimeSlots.map(timeSlot => day+timeSlot.time)])
+        }
+    }
+
 
     return <div className='grid grid-cols-[repeat(7,24px)] grid-rows-[repeat(14,24px)] gap-1 text-center text-sm'>
         <div className='w-4 h-4'></div>
-        {days.map(day => (<div className='' key={day}>{day}</div>))}
+        {days.map(day => (
+            <div 
+                key={day} 
+                className='rounded-md hover:bg-gray-100 cursor-pointer'
+                onClick={handleSelectColumn(day)}
+            >{day}</div>
+        ))}
         {scheduleTimeSlots.map(timeSlot => [
-            <div key={timeSlot.time}>{timeSlot.time}</div>,
+            <div 
+                key={timeSlot.time} 
+                className='rounded-md hover:bg-gray-100 cursor-pointer'
+                onClick={handleSelectRow(timeSlot.time)}>{timeSlot.time}
+            </div>,
             ...days.map(day => (
                 <div 
                     key={day+timeSlot.time}
