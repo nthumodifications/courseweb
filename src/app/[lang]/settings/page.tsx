434c1f4df41253bf657a975f9da293af03d45999
ImJustChew <chewtzihwee@gmail.com>
2023-10-30 23:33:38 +0800

    fixed TimetableThemePreview Light mode typo

--- src/app/[lang]/settings/page.tsx
+++ src/app/[lang]/settings/page.tsx
@@ -8,7 +8,7 @@ import {  useEffect, useState } from "react";
 const TimetableThemePreview = ({ theme, onClick = () => {}, selected = false }: { theme: string, selected?: boolean, onClick?: () => void}) => {
     return <div 
         onClick={onClick}
-        className={`flex flex-col rounded-lg p-3 hover:dark:bg-neutral-800 hover:bg-gray-100 transition cursor-pointer space-y-2 ${selected? "bg-gray-100 dark: bg-neutral-800":""}`}>
+        className={`flex flex-col rounded-lg p-3 hover:dark:bg-neutral-800 hover:bg-gray-100 transition cursor-pointer space-y-2 ${selected? "bg-gray-100 dark:bg-neutral-800":""}`}>
         <div className="flex flex-row">
             {timetableColors[theme].map((color, index) => (
                 <div className="flex-1 h-6 w-6" style={{background: color}} key={index}/>
