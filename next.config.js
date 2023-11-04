a19842cea4c3acd5630807803e985bd91ee69a22
ImJustChew <chewtzihwee@gmail.com>
2023-11-05 03:56:10 +0800

    added caching for start url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const withPWA = require('@ducanh2912/next-pwa').default({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
+    cacheStartUrl: true,
+    dynamicStartUrlRedirect: '/zh/today',
 })
   
 /** @type {import('next').NextConfig} */
