afdbabb89f392c95f4a7b8d0ebf0c3f440702195
ImJustChew <chewtzihwee@gmail.com>
2023-11-06 16:43:52 +0800

    Weather is broken for some reason :(

--- src/lib/weather.ts
+++ src/lib/weather.ts
@@ -1,14 +1,14 @@
 'use server';
 
 export async function getWeatherData() {
-    const res = await fetch(
-      `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-055?Authorization=${process.env.CWA_API_KEY}&format=JSON&locationName=%E6%9D%B1%E5%8D%80&elementName=Wx,WeatherDescription` ,
-      { 
-        next: { revalidate: 60 * 60 * 24 }
-      })
+  const res = await fetch(
+    `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-055?Authorization=${process.env.CWA_API_KEY}&format=JSON&locationName=%E6%9D%B1%E5%8D%80&elementName=Wx,WeatherDescription`,
+    {
+      next: { revalidate: 60 * 60, tags: ['weather'] },
+    })
 
-    if (!res.ok) {
-      throw new Error('Failed to fetch data')
-    }
-    return res.json()
+  if (!res.ok) {
+    throw new Error('Failed to fetch data ' + res.status + await res.text())
+  }
+  return res.json()
 }
