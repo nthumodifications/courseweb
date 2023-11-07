c7b9561391f109550d2239eb665bd5dd84a81803
ImJustChew <chewtzihwee@gmail.com>
2023-11-07 23:33:00 +0800

    BREAKING CHANGES: migrate supabase to supabase cloud

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,7 +1,7 @@
 import {Json, Database} from '@/types/supabase';
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL ?? "", process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "");
+const supabase = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL ?? "", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "");
 
 export type CourseDefinition = Database['public']['Tables']['courses']['Row'];
 export type CourseSyllabusDefinition = Database['public']['Tables']['course_syllabus']['Row'];
