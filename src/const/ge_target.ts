3d4672a54917412aef03749c53b32bc426938be6
ImJustChew <chewtzihwee@gmail.com>
2023-11-07 21:40:23 +0800

    const(ge_target): removed stale ge_targets

--- src/const/ge_target.ts
+++ src/const/ge_target.ts
@@ -1,3 +1,12 @@
+/**
+ * @file 通識課程目標代碼
+ * @module src/const/ge_target.ts
+ * @version 1.0.0
+ * @see https://curricul.site.nthu.edu.tw/p/404-1208-11133.php
+ * @description 通識課程目標代碼, 除了 *1, *3, *7 以外, 其他代碼已經不再使用
+ */
+
+
 export const GETargetCodes = [
     {
         code: '*1',
@@ -6,13 +15,13 @@ export const GETargetCodes = [
         description_zh: '各系學生可做為通識學分課程',
         description_en: 'For all students all credits are accepted as General Education.'
     },
-    {
-        code: '*2',
-        short_zh: '限人文社會學院學生',
-        short_en: 'Only HSS students',
-        description_zh: '限人文社會學院學生做為通識學分課程，其他（院）系學生不得選修。',
-        description_en: 'Credits are accepted as General Education but only for students of Humanities and Social Sciences.'
-    },
+    // {
+    //     code: '*2',
+    //     short_zh: '限人文社會學院學生',
+    //     short_en: 'Only HSS students',
+    //     description_zh: '限人文社會學院學生做為通識學分課程，其他（院）系學生不得選修。',
+    //     description_en: 'Credits are accepted as General Education but only for students of Humanities and Social Sciences.'
+    // },
     {
         code: '*3',
         short_zh: '開課系所/院學生非通識',
@@ -20,27 +29,27 @@ export const GETargetCodes = [
         description_zh: '全校學生皆可選修；但開課系(所)所屬(院)學生選修，不列入通識課程學分',
         description_en: 'For all students the credits are accepted as General Education but not for the students registered in the same departments or colleges which offer the courses.'
     },
-    {
-        code: '*4',
-        short_zh: '限非開課系所/院學生',
-        short_en: 'Only students not registered in the department/college',
-        description_zh: '開課系（所）所屬學院學生不可選修，其他（院）系學生可做為通識學分課程。',
-        description_en: 'Only selectable for students who are not registered in the department or college which offer the courses and credits are accepted as General Education.'
-    },
-    {
-        code: '*5',
-        short_zh: '限非開課系所學生',
-        short_en: 'Only students not registered in the department',
-        description_zh: '開課系(所)之學生不可選修，其他系(所)學生可選修',
-        description_en: 'Only selectable for students who are not registered in the department which offers the courses and credits are accepted as General Education. '
-    },
-    {
-        code: '*6',
-        short_zh: '師資培育中心學生非通識',
-        short_en: 'Not GE for Teacher Education Center Students',
-        description_zh: '師資培育中心開設課程，非修習教育學程學生可選修且列入通識課程學分',
-        description_en: 'Students not registered for the teacher education program can take courses marked with *6 offered by the Teacher Education Center, and the credits are accepted as General Education. '
-    },
+    // {
+    //     code: '*4',
+    //     short_zh: '限非開課系所/院學生',
+    //     short_en: 'Only students not registered in the department/college',
+    //     description_zh: '開課系（所）所屬學院學生不可選修，其他（院）系學生可做為通識學分課程。',
+    //     description_en: 'Only selectable for students who are not registered in the department or college which offer the courses and credits are accepted as General Education.'
+    // },
+    // {
+    //     code: '*5',
+    //     short_zh: '限非開課系所學生',
+    //     short_en: 'Only students not registered in the department',
+    //     description_zh: '開課系(所)之學生不可選修，其他系(所)學生可選修',
+    //     description_en: 'Only selectable for students who are not registered in the department which offers the courses and credits are accepted as General Education. '
+    // },
+    // {
+    //     code: '*6',
+    //     short_zh: '師資培育中心學生非通識',
+    //     short_en: 'Not GE for Teacher Education Center Students',
+    //     description_zh: '師資培育中心開設課程，非修習教育學程學生可選修且列入通識課程學分',
+    //     description_en: 'Students not registered for the teacher education program can take courses marked with *6 offered by the Teacher Education Center, and the credits are accepted as General Education. '
+    // },
     {
         code: '*7',
         short_zh: '開課系所學生非通識',
