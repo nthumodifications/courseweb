645ba5fc624b98a2c9141116e57e58ad8492494d
ImJustChew <chewtzihwee@gmail.com>
2023-11-08 23:28:38 +0800

    sql: removed sql

--- database/functions/time_slots.sql
+++ database/functions/time_slots.sql
@@ -1,6 +1,7 @@
-create function time_slots(courses_with_syllabus) returns text[] as $$
-  select split_times($1.times);
-$$ language sql immutable;
+-- we now split in the prev function alr. so this is not needed anymore
+-- create function time_slots(_courses_with_syllabus) returns text[] as $$
+--   select split_times($1.times);
+-- $$ language sql immutable;
 
 create function cds_time_slots(cds_courses) returns text[] as $$
   select split_times($1.times);
---
+++ database/views/distinct_semester.sql
@@ -0,0 +1,2 @@
+create or replace view distinct_semesters as
+SELECT DISTINCT semester from courses
