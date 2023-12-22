71f3b8b4071788b977bd7d3186e99b3b7d5a7c89
ImJustChew <chewtzihwee@gmail.com>
2023-12-23 01:45:03 +0800

    feat: add semester on course name

--- src/app/[lang]/courses/page.tsx
+++ src/app/[lang]/courses/page.tsx
@@ -1,10 +1,10 @@
 'use client';;
 import CourseListItem from "@/components/Courses/CourseListItem";
 import InputControl from "@/components/FormComponents/InputControl";
-import supabase, {CourseDefinition, CourseSyllabusView} from '@/config/supabase';
-import { Button, CircularProgress, Divider, Drawer, IconButton, LinearProgress, Stack } from "@mui/joy";
+import supabase, { CourseSyllabusView } from '@/config/supabase';
+import { Button, CircularProgress, Divider, Drawer, IconButton, Stack } from "@mui/joy";
 import { NextPage } from "next";
-import { useEffect, useState, Fragment, useRef, use, useMemo } from "react";
+import { useEffect, useState, Fragment, useRef, useMemo } from "react";
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, Filter, Search, X } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { useDebouncedCallback } from "use-debounce";
@@ -14,7 +14,6 @@ import RefineControls from '@/components/Courses/RefineControls';
 import useDictionary from "@/dictionaries/useDictionary";
 import { useRouter, useSearchParams } from "next/navigation";
 import queryString from 'query-string';
-import { GETargetCodes } from "@/const/ge_target";
 import { departments } from "@/const/departments";
 import useUserTimetable from "@/hooks/useUserTimetable";
 import {Department} from '@/types/courses';
@@ -286,7 +285,7 @@ const CoursePage: NextPage = () => {
                     {loading && <div className="absolute inset-0 bg-white/60 dark:bg-neutral-900/60 z-10"></div>}
                     <div className="flex flex-col w-full h-full space-y-4 pb-8">
                         <div className="flex flex-row justify-between px-3 py-1 border-b dark:border-neutral-800">
-                            <h6 className="text-gray-600 dark:text-neutral-400">{dict.course.list.courses}</h6>
+                            <h6 className="text-gray-600 dark:text-neutral-400">{filters.semester} {dict.course.list.courses}</h6>
                             <h6 className="text-gray-600 dark:text-neutral-400">{dict.course.list.found}: {totalCount} {dict.course.list.courses}</h6>
                         </div>
                         <div className="flex flex-col w-full h-full space-y-5">
