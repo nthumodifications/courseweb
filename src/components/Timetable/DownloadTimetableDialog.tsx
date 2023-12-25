3505dc9f9c40a4c4d08d73912b879a206a9c37cc
ImJustChew <chewtzihwee@gmail.com>
2023-12-26 04:23:36 +0800

    chore: make timetable slightly fatter for all screens

--- src/components/Timetable/DownloadTimetableDialog.tsx
+++ src/components/Timetable/DownloadTimetableDialog.tsx
@@ -48,8 +48,10 @@ const DownloadTimetableComponent = () => {
             loading={loading}
         >{dict.dialogs.DownloadTimetableDialog.buttons.image}</Button>
         <div className='relative overflow-hidden'>
-            <div className='absolute h-[915px] w-[414px] px-2 pt-4 pb-8 grid place-items-center bg-white dark:bg-neutral-900' ref={ref}>
-                <Timetable timetableData={timetableData} vertical/>
+            <div className='absolute h-[915px] w-[539px] px-2 pt-4 pb-8 grid place-items-center bg-white dark:bg-neutral-900' ref={ref}>
+                <div className='h-[915px] w-[414px]'>
+                    <Timetable timetableData={timetableData} vertical/>
+                </div>
             </div>
         </div>
     </>
