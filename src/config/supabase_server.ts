5328da96b6d2c79dacd10f87024c9f19b3db393b
ImJustChew <chewtzihwee@gmail.com>
2024-01-03 20:07:20 +0800

    BREAKING: make supabase_server server-only

--- src/config/supabase_server.ts
+++ src/config/supabase_server.ts
@@ -1,5 +1,6 @@
 import { Database } from '@/types/supabase';
 import { createClient } from '@supabase/supabase-js';
+import 'server-only';
 
 const supabase_server = createClient<Database>(process.env.SUPABASE_URL ?? "", process.env.SUPABASE_SERVICE_ROLE_KEY ?? "");
 
