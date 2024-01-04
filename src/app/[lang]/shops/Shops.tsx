cc3724c4bd2604944c71a15957d90c849fe0c0d5
Joshimello <joshualeanjw@gmail.com>
2024-01-04 16:44:51 +0800

    .

--- src/app/[lang]/shops/Shops.tsx
+++ src/app/[lang]/shops/Shops.tsx
@@ -11,25 +11,34 @@ import { useState, useEffect } from 'react'
 
 const Shops = ({ data }) => {
 
-  return (<div>
-    
-    {/*{data.map(([area, image, name, note, phone, schedule], index) => 
-      <div key={name || index} className="p-8">
-        <Card>
-          <CardHeader>
-            <CardTitle>{name}</CardTitle>
-            <CardDescription>{area}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+  return (
+    <div className="grid md:grid-cols-[1fr_200px] gap-6 p-6">
+      <div className="flex flex-col gap-4">
+        {data.map(({ area, image, name, note, phone, schedule }, index) => 
+          <div key={name || index}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{name}</CardTitle>
+                <CardDescription>{area}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
+        )}
       </div>
-    )}*/}
-  </div>)
+      <div className="flex flex-col gap-4 w-[200px]">
+        <div className="flex flex-col gap-2">
+          <span className="text-sm font-medium">Location</span>
+          
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Shops
