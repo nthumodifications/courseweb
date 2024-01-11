4f8abcdd40d0dc5fb371ee4c0c77ffabde5bc2bd
Joshimello <joshualeanjw@gmail.com>
2024-01-12 00:36:51 +0800

    draft

--- src/app/[lang]/shops/Shops.tsx
+++ src/app/[lang]/shops/Shops.tsx
@@ -8,6 +8,15 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { useState, useEffect } from 'react'
+import { MapPin, Phone, Info } from 'lucide-react'
+
+const getDay = () => {
+  const now = new Date()
+  const day = now.getDay()
+  if(day == 0) return 'sunday'
+  if(day == 6) return 'saturday'
+  return 'weekday'
+}
 
 const Shops = ({ data }) => {
 
@@ -16,25 +25,40 @@ const Shops = ({ data }) => {
       <div className="flex flex-col gap-4">
         {data.map(({ area, image, name, note, phone, schedule }, index) => 
           <div key={name || index}>
-            <Card>
-              <CardHeader>
-                <CardTitle>{name}</CardTitle>
-                <CardDescription>{area}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
+            <Card className="overflow-hidden flex">
+              <div className="bg-blue-500 w-2"></div>
+              <div className="w-full p-6 grid grid-cols-6 gap-4">
+                <div className="">
+                  <span className="bold">{schedule[getDay()]}</span>                
+                </div>
+                <div className="col-span-2 flex items-center gap-4">
+                  <img className="h-16 w-16 rounded-full object-cover" src={image} />
+                  <div className="flex flex-col">
+                    <span className="font-bold text-lg">{name}</span>
+                    <div className="flex items-center gap-1">
+                      <MapPin size="16" />                    
+                      <span className="text-sm">{area}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-2 flex flex-col">
+                  <div className="flex items-center gap-1">
+                    <Phone size="16" />
+                    <span className="text-sm">{phone}</span>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Info size="16" />
+                    <span className="text-sm">{note}</span>
+                  </div>
+                </div>
+              </div>
             </Card>
           </div>
         )}
       </div>
       <div className="flex flex-col gap-4 w-[200px]">
         <div className="flex flex-col gap-2">
-          <span className="text-sm font-medium">Location</span>
-          
+          <span className="text-sm font-medium">Filter</span>
         </div>
       </div>
     </div>
