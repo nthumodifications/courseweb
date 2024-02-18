503a40c9b6cffba38e5d3f364e659edfe6648f59
ImJustChew <chewtzihwee@gmail.com>
2024-02-19 02:07:21 +0800

    added back manifest icons for pwa installability

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,10 +15,50 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#7e1083',
         icons: [
             {
-              src: '/favicon.ico',
-              sizes: 'any',
-              type: 'image/x-icon',
+                src: '/favicon.ico',
+                sizes: 'any',
+                type: 'image/x-icon',
             },
-        ],
+            {
+                "src": "images/icons/icon-72x72.png",
+                "sizes": "72x72",
+                "type": "image/png"
+            },
+            {
+                "src": "images/icons/icon-96x96.png",
+                "sizes": "96x96",
+                "type": "image/png"
+            },
+            {
+                "src": "images/icons/icon-128x128.png",
+                "sizes": "128x128",
+                "type": "image/png"
+            },
+            {
+                "src": "images/icons/icon-144x144.png",
+                "sizes": "144x144",
+                "type": "image/png"
+            },
+            {
+                "src": "images/icons/icon-152x152.png",
+                "sizes": "152x152",
+                "type": "image/png"
+            },
+            {
+                "src": "images/icons/icon-192x192.png",
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": "images/icons/icon-384x384.png",
+                "sizes": "384x384",
+                "type": "image/png"
+            },
+            {
+                "src": "images/icons/icon-512x512.png",
+                "sizes": "512x512",
+                "type": "image/png"
+            }
+        ]
     }
 }
