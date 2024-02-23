bf1ddb309d1b36403048f06cd1a0e8d5a8c00309
ImJustChew <chewtzihwee@gmail.com>
2024-02-23 20:11:58 +0800

    added fake directory to fake webDir

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nthumods.courseweb',
   appName: 'NTHUMods',
-  webDir: 'out',
+  webDir: 'fakeout',
   server: {
     url: 'https://nthumods.com',
     androidScheme: 'https',
