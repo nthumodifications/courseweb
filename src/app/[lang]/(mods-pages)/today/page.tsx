774ccd5a1b3f385459077da044d90742a219fd86
walt-tsai <112289856+walt-tsai@users.noreply.github.com>
2024-03-05 20:50:35 +0800

    Feat: show events from NTHU calendar in todays page

--- src/app/[lang]/(mods-pages)/today/page.tsx
+++ src/app/[lang]/(mods-pages)/today/page.tsx
@@ -4,12 +4,13 @@ import TodaySchedule from "@/components/Today/TodaySchedule";
 import { useQuery } from "@tanstack/react-query";
 import {WeatherData} from '@/types/weather';
 import { AlertDefinition } from "@/config/supabase";
+import {EventData} from "@/types/calendar_event";
 
 
 const TodayPage: NextPage = () => {
 
     const { data: weatherData, error: weatherError, isLoading: weatherLoading } = useQuery<WeatherData>({
-        queryKey: ['weather'], 
+        queryKey: ['weather'],
         queryFn: async () => {
             const res = await fetch('/api/dashboard/weather');
             const data = await res.json();
@@ -18,7 +19,7 @@ const TodayPage: NextPage = () => {
     });
 
     const { data: alerts = [], error: alertError, isError: alertLoading } = useQuery<AlertDefinition[]>({
-        queryKey: ['alert'], 
+        queryKey: ['alert'],
         queryFn: async () => {
             const res = await fetch('/api/dashboard/alert');
             const data = await res.json();
@@ -26,9 +27,18 @@ const TodayPage: NextPage = () => {
         }
     });
 
+    const {data: calendarData = [], error: calendarError, isLoading: calendarLoading } = useQuery<EventData[]>({
+        queryKey: ['event'],
+        queryFn: async () => {
+            const res = await fetch('/api/dashboard/calendar');
+            const data = await res.json();
+            return data;
+        }
+    });
+
     return (
         <div className="h-full grid grid-cols-1 md:grid-cols-[380px_auto] md:grid-rows-1">
-            <TodaySchedule weather={weatherData} weatherLoading={weatherLoading} alerts={alerts} alertLoading={alertLoading}/>
+            <TodaySchedule weather={weatherData} weatherLoading={weatherLoading} alerts={alerts} alertLoading={alertLoading} calendar={calendarData} calendarLoading={calendarLoading}/>
             <main className='overflow-auto'>
                 {/* {children} */}
             </main>
