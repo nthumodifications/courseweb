fcd4fe2907741b456ba93408f349f4813bfb9e09
ImJustChew <chewtzihwee@gmail.com>
2024-03-21 01:45:41 +0800

    ui: shadify downloadsyllabus

--- src/app/[lang]/(mods-pages)/courses/[courseId]/DownloadSyllabus.tsx
+++ src/app/[lang]/(mods-pages)/courses/[courseId]/DownloadSyllabus.tsx
@@ -1,14 +1,13 @@
-import supabase from "@/config/supabase";
+import { Button } from "@/components/ui/button";
 import { RawCourseID } from "@/types/courses";
-import { Button } from "@mui/joy";
-import Link from "next/link";
 import { DownloadCloud } from "lucide-react";
+import Link from "next/link";
 
 const DownloadSyllabus = ({ courseId }: { courseId: RawCourseID }) => {
     const fileURL = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/syllabus/${encodeURIComponent(courseId)}.pdf`;
 
     return <Link href={fileURL} target="_blank">
-        <Button variant="outlined" startDecorator={<DownloadCloud />}>下載 PDF</Button>
+        <Button variant="outline" ><DownloadCloud className="h-4 w-4 mr-2"/> 下載 PDF</Button>
     </Link>
 }
 
