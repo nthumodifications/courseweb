5245a0a1bb481faa4aa9729b1d19b9769c44cab3
ImJustChew <chewtzihwee@gmail.com>
2024-03-21 01:20:24 +0800

    deps: bump sentry, remove old deps

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -23,10 +23,5 @@ Sentry.init({
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
-    new Sentry.Replay({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
   ],
 });
