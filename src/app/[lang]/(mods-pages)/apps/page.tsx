fe6c8a9cd4ea7af86807df093eb343a71cdcf9a9
ImJustChew <chewtzihwee@gmail.com>
2024-04-01 00:15:39 +0800

    fix: improve stuff

--- src/app/[lang]/(mods-pages)/apps/page.tsx
+++ src/app/[lang]/(mods-pages)/apps/page.tsx
@@ -22,8 +22,8 @@ const AppList = ({
         <div className="h-full w-full">
             <div className="flex flex-col">
                 <h1 className="text-xl font-bold px-4 py-2">{dict.applist.title}</h1>
-                {apps.filter(m => m.ais ? !!ais.enabled: true ).map(app => (<div key={app.id} className="flex flex-row items-center space-x-2 py-2 px-4 hover:bg-gray-100 dark:hover:bg-neutral-800">
-                    <Link href={app.href} className="flex flex-row flex-1 items-center space-x-2" target={app.target}>
+                {apps.filter(m => false ? !!ais.enabled: true ).map(app => (<div key={app.id} className="flex flex-row items-center space-x-2 py-2 px-4 hover:bg-gray-100 dark:hover:bg-neutral-800">
+                    <Link href={app.href} className="flex flex-row flex-1 items-center space-x-2">
                         <div className="p-3 rounded-full bg-indigo-100 text-indigo-800 grid place-items-center">
                             <app.Icon size={16}/>
                         </div>
