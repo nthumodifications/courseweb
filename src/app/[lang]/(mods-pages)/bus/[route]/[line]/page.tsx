d7980d227ab2f22e56bb635bb8616f167f745262
ImJustChew <chewtzihwee@gmail.com>
2024-05-09 23:57:23 +0800

    fix: line details now has line details

--- src/app/[lang]/(mods-pages)/bus/[route]/[line]/page.tsx
+++ src/app/[lang]/(mods-pages)/bus/[route]/[line]/page.tsx
@@ -1,6 +1,11 @@
 'use client';
+import { GreenLineIcon } from "@/components/BusIcons/GreenLineIcon";
+import { NandaLineIcon } from "@/components/BusIcons/NandaLineIcon";
+import { RedLineIcon } from "@/components/BusIcons/RedLineIcon";
 import { Button } from "@/components/ui/button";
+import useDictionary from "@/dictionaries/useDictionary";
 import { getTimeOnDate } from "@/helpers/bus";
+import { useSettings } from "@/hooks/contexts/settings";
 import useTime from "@/hooks/useTime";
 import { cn } from "@/lib/utils";
 import { addMinutes, formatDate, isSameMinute, subMinutes } from "date-fns";
@@ -17,6 +22,7 @@ enum BusStationState {
 
 const linesDict: {
     [key: string]: {
+        Icon: React.FC,
         title_zh: string,
         title_en: string,
         stations_zh: string[],
@@ -34,42 +40,48 @@ const linesDict: {
     // (綠線) 台積館 → 人社院/生科院 → 楓林小徑 → 綜二館 → 北校門口",
     // (Red Line) TSMC Building → South Gate Parking Lot → Yi Pavilion Parking Lot → Maple Path → General Building II → North Main Gate
     // (Green Line) TSMC Building → CHSS/CLS Building → Maple Path → General Building II → North Main Gate"
-    'green_up' : {
+    'green_up': {
+        Icon: GreenLineIcon,
         'title_zh': "綠線 往台積館",
         'title_en': "Green Line To TSMC",
         'stations_zh': ['北校門口', '綜二', '楓林小徑', '奕園停車場', '南門停車場', '台積館'],
         'stations_en': ['North Main Gate', 'General Building II', 'Maple Path', 'Yi Pavilion Parking Lot', 'South Gate Parking Lot', 'TSMC Building'],
         'timings': [0, 2, 1, 2, 1],
     },
-    'green_down' : {
+    'green_down': {
+        Icon: GreenLineIcon,
         title_zh: "綠線 往北校門口",
         title_en: "Green Line To North Gate",
         'stations_zh': ['台積館', '人社院/生科院', '楓林小徑', '綜二', '北校門口'],
         'stations_en': ['TSMC Building', 'South Gate Parking Lot', 'Yi Pavilion Parking Lot', 'Maple Path', 'General Building II', 'North Main Gate'],
         'timings': [0, 2, 1, 2, 1],
     },
-    'red_up' : {
+    'red_up': {
+        Icon: RedLineIcon,
         title_zh: "紅線 往台積館",
         title_en: "Red Line to TSMC",
         'stations_zh': ['北校門口', '綜二', '楓林小徑', '人社院/生科院', '台積館'],
         'stations_en': ['North Main Gate', 'General Building II', 'Maple Path', 'CHSS/CLS Building', 'TSMC Building'],
         'timings': [0, 2, 1, 2, 1],
     },
-    'red_down' : {
+    'red_down': {
+        Icon: RedLineIcon,
         title_zh: "紅線 往北校門口",
         title_en: "Red Line to North Gate",
         'stations_zh': ['台積館', '南門停車場', '奕園停車場', '楓林小徑', '綜二', '北校門口'],
         'stations_en': ['TSMC Building', 'South Gate Parking Lot', 'Yi Pavilion Parking Lot', 'Maple Path', 'General Building II', 'North Main Gate'],
         'timings': [0, 2, 1, 2, 1],
     },
-    'nanda_up' : {
+    'nanda_up': {
+        Icon: NandaLineIcon,
         title_zh: "南大校車 往南大校區",
         title_en: "Nanda Line To Nanda",
         'stations_zh': ['北校門口', '綜二', '人社院/生科院', '台積館', '南大校區'],
         'stations_en': ['Nanda Line', 'General Building II', 'CHSS/CLS Building', 'TSMC Building', 'Nanda Campus'],
         'timings': [0, 2, 1, 2, 10],
     },
-    'nanda_down' : {
+    'nanda_down': {
+        Icon: NandaLineIcon,
         title_zh: "南大校車 往校本部",
         title_en: "Nanda Line to Main Campus",
         'stations_zh': ['南大校區', '台積館', '人社院/生科院', '綜二', '北校門口'],
@@ -83,30 +95,37 @@ const LineDisplayPage = () => {
     const searchParams = useSearchParams();
     const start_time = searchParams.get('start_time');
     const time = useTime();
-    if(!(line in linesDict)) return (<div>Invalid Line</div>);
+    if (!(line in linesDict)) return (<div>Invalid Line</div>);
     const lineData = linesDict[line] as typeof linesDict['green_up'];
+    const { language } = useSettings();
+    const dict = useDictionary();
     const displayText = useMemo<{
         state: BusStationState,
         station: string,
         time: string,
     }[]>(() => {
         const startDate = getTimeOnDate(time, start_time as string);
-        return lineData.stations_zh.map((station, i) => {
+        return (language == 'zh' ?lineData.stations_zh : lineData.stations_en).map((station, i) => {
             const stationDepTime = addMinutes(startDate, lineData.timings.slice(0, i).reduce((a, b) => a + b, 0));
-            if(time < stationDepTime) return {state: BusStationState.UNAVAILABLE, station, time: formatDate(stationDepTime, 'HH:mm')};
-            if(isSameMinute(time, stationDepTime)) return {state: BusStationState.AT_STATION, station, time: '即將進站'};
-            if(time > stationDepTime) return {state: BusStationState.LEFT, station, time: formatDate(stationDepTime, 'HH:mm')};
-            return {state: BusStationState.UNAVAILABLE, station, time: formatDate(stationDepTime, 'HH:mm')};
+            if (time < stationDepTime) return { state: BusStationState.UNAVAILABLE, station, time: formatDate(stationDepTime, 'HH:mm') };
+            if (isSameMinute(time, stationDepTime)) return { state: BusStationState.AT_STATION, station, time: dict.bus.departing };
+            if (time > stationDepTime) return { state: BusStationState.LEFT, station, time: formatDate(stationDepTime, 'HH:mm') };
+            return { state: BusStationState.UNAVAILABLE, station, time: formatDate(stationDepTime, 'HH:mm') };
         });
     }, [time, start_time]);
 
     return <div className="flex flex-col gap-2">
-        <div className="">
+        <div className="flex flex-row items-center px-2 gap-4">
             <Button variant={'ghost'} asChild>
                 <Link href='/bus'>
                     <ChevronLeft className="w-4 h-4 mr-2" />
                 </Link>
             </Button>
+            <div className="flex flex-row gap-4 items-center">
+                <lineData.Icon />
+                <h3 className="text-slate-800 dark:text-neutral-200 font-bold">{language == 'zh' ? lineData.title_zh: lineData.title_en}</h3>
+            </div>
+
         </div>
         <div className="w-full items-start inline-flex px-4">
             <div className="w-full p-2 flex-col justify-start inline-flex">
@@ -114,13 +133,13 @@ const LineDisplayPage = () => {
                     <div className="h-auto relative w-3">
                         {i != 0 && (m.state > BusStationState.UNAVAILABLE ?
                             <div className="absolute top-0 left-[calc(50%-2px)] w-1 h-1/2 bg-nthu-400 z-10" />
-                            :<div className="absolute top-0 left-[calc(50%-2px)] w-1 h-1/2 bg-slate-200 z-10" />)}
-                        {m.state >= BusStationState.AT_STATION ? 
+                            : <div className="absolute top-0 left-[calc(50%-2px)] w-1 h-1/2 bg-slate-200 z-10" />)}
+                        {m.state >= BusStationState.AT_STATION ?
                             <div className="absolute top-[calc(50%-6px)] w-3 h-3 bg-nthu-500 rounded-full z-20" />
-                            :<div className="absolute top-[calc(50%-6px)] w-3 h-3 bg-slate-200 rounded-full z-20" />}
+                            : <div className="absolute top-[calc(50%-6px)] w-3 h-3 bg-slate-200 rounded-full z-20" />}
                         {i != (displayText.length - 1) && (m.state > BusStationState.AT_STATION ?
                             <div className="absolute top-1/2 left-[calc(50%-2px)] w-1 h-1/2 bg-nthu-400 z-10" />
-                            :<div className="absolute top-1/2 left-[calc(50%-2px)] w-1 h-1/2 bg-slate-200 z-10" />)}
+                            : <div className="absolute top-1/2 left-[calc(50%-2px)] w-1 h-1/2 bg-slate-200 z-10" />)}
                     </div>
                     <div className={cn("flex-1 py-4 justify-start items-center gap-2 flex border-b border-border", m.state > BusStationState.AT_STATION ? 'opacity-30' : '')}>
                         <div className="text-slate-800 dark:text-slate-200 text-base font-bold">{m.station}</div>
