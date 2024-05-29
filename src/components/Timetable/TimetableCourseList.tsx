9db773a745c3087ca4b4a745653a75021a122e21
ImJustChew <chewtzihwee@gmail.com>
2024-05-30 03:34:29 +0800

    fix: courses might be empty, default to []

--- src/components/Timetable/TimetableCourseList.tsx
+++ src/components/Timetable/TimetableCourseList.tsx
@@ -155,8 +155,8 @@ const TimetableCourseList = ({ vertical, setVertical }: { vertical: boolean, set
 
 
     const shareLink = `https://nthumods.com/timetable/view?${Object.keys(courses).map(sem => `semester_${sem}=${courses[sem].map(id => encodeURI(id)).join(',')}`).join('&')}&colorMap=${encodeURIComponent(JSON.stringify(colorMap))}`
-    const webcalLink = `webcals://nthumods.com/timetable/calendar.ics?semester=${semester}&${`semester_${semester}=${courses[semester].map(id => encodeURI(id)).join(',')}`}`
-    const icsfileLink = `https://nthumods.com/timetable/calendar.ics?semester=${semester}&${`semester_${semester}=${courses[semester].map(id => encodeURI(id)).join(',')}`}`
+    const webcalLink = `webcals://nthumods.com/timetable/calendar.ics?semester=${semester}&${`semester_${semester}=${(courses[semester] ?? []).map(id => encodeURI(id)).join(',')}`}`
+    const icsfileLink = `https://nthumods.com/timetable/calendar.ics?semester=${semester}&${`semester_${semester}=${(courses[semester] ?? []).map(id => encodeURI(id)).join(',')}`}`
 
     const displayCourseData = useMemo(() => getSemesterCourses(semester), [getSemesterCourses, semester]);
 
