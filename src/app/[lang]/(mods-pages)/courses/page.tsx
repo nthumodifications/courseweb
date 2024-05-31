4f3fe23fc01bcf4c160dc79c2778151d68364200
ImJustChew <chewtzihwee@gmail.com>
2024-06-01 03:13:42 +0800

    feat: courses will now open full dialog

--- src/app/[lang]/(mods-pages)/courses/page.tsx
+++ src/app/[lang]/(mods-pages)/courses/page.tsx
@@ -1,19 +1,24 @@
-'use client'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { useState } from 'react';
+'use client';
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { useEffect, useState } from 'react';
 import CourseSearchContainer from './CourseSearchContainer';
-import { Separator } from "@/components/ui/separator";
+import { useRouter } from "next/navigation";
 
 const CourseDialog = () => {
   const [open, setOpen] = useState(true);
+  const router = useRouter();
+
+  //first load open dialog
+  useEffect(() => {
+    setOpen(true);
+  }, [])
+
+  //if closed, navigate to timetable page
+  useEffect(() => {
+    if (!open) {
+      router.push('/timetable')
+    }
+  }, [open])
   
   return (
     <Dialog open={open} onOpenChange={setOpen}>
