adc87b968a9940aff5d85add786c7140210cc010
ImJustChew <chewtzihwee@gmail.com>
2024-06-10 17:16:25 +0800

    fix: move headless ais behind api routes

---
+++ src/app/api/ais_auth/refresh/route.ts
@@ -0,0 +1,12 @@
+import { refreshUserSession, signInToCCXP } from "@/lib/headless_ais";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+    const form = await req.formData();
+    const studentid = form.get("studentid");
+    const encryptedPassword = form.get("encryptedPassword");
+
+    if(!studentid || !encryptedPassword) return NextResponse.json({ error: { message: "Missing Student ID and Password" }}, { status: 400 })
+
+    return await refreshUserSession(studentid as string, encryptedPassword as string);
+}
