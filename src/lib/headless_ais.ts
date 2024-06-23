7e99e39ddebdef24cd836b263a2c778435380fd0
ImJustChew <chewtzihwee@gmail.com>
2024-06-24 00:02:04 +0800

    retry

--- src/lib/headless_ais.ts
+++ src/lib/headless_ais.ts
@@ -96,21 +96,23 @@ export const signInToCCXP = async (studentid: string, password: string): SignInT
                     const res = await fetchWithTimeout(url, {
                         timeout: 3000,
                         "headers": {
-                            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                             "accept-language": "en-US,en;q=0.9",
                             "cache-control": "max-age=0",
-                            "content-type": "application/x-www-form-urlencoded",
-                            "sec-ch-ua": "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Microsoft Edge\";v=\"110\"",
+                            "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Microsoft Edge\";v=\"126\"",
                             "sec-ch-ua-mobile": "?0",
                             "sec-ch-ua-platform": "\"Windows\"",
                             "sec-fetch-dest": "document",
                             "sec-fetch-mode": "navigate",
-                            "sec-fetch-site": "same-origin",
-                            "upgrade-insecure-requests": "1",
+                            "sec-fetch-site": "none",
+                            "sec-fetch-user": "?1",
+                            "upgrade-insecure-requests": "1"
                         },
                         keepalive: true,
                         "body": null,
                         "method": "GET",
+                        "mode": "cors",
+                        "credentials": "include"
                         
                     });
                     
