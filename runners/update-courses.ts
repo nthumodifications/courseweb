af81b4174d0117dc5896368b569219b753c612bc
ImJustChew <chewtzihwee@gmail.com>
2024-07-25 02:36:42 +0800

    feat: finalized scrape cousres pipeline

--- runners/update-courses.ts
+++ runners/update-courses.ts
@@ -1,9 +1,23 @@
 import schedule from 'node-schedule';
 
-const job = schedule.scheduleJob('8 * * * *', async () => {
+// Run every day at 8:00 AM
+// 8 AM (GMT+8) = 0 AM (GMT)
+const job = schedule.scheduleJob('0 0 * * *', async () => {
     try {
         console.log('syncing courses begin uwu')
         const res = await fetch('http://localhost:8080/api/scrape-archived-courses?semester=11310', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${process.env.CRON_SECRET}`
+            }
+        });
+        const res2 = await fetch('http://localhost:8080/api/scrape-syllabus?semester=11310', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${process.env.CRON_SECRET}`
+            }
+        });
+        const res3 = await fetch('http://localhost:8080/api/sync-algolia?semester=11310', {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${process.env.CRON_SECRET}`
