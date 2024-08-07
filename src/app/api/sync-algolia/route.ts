fc50cb21cd8b9c7c5b5b58b90281bf79e06ee030
Chew Tzi Hwee <chewtzihwee@gmail.com>
2024-08-07 13:59:03 +0800

    Update route.ts

--- src/app/api/sync-algolia/route.ts
+++ src/app/api/sync-algolia/route.ts
@@ -3,13 +3,6 @@ import { syncCoursesToAlgolia } from '@/lib/scrapers/course';
 
 
 export const GET = async (request: NextRequest) => {
-    const authHeader = request.headers.get('authorization');
-    if (process.env.NODE_ENV == 'production' && authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-      return new Response('Unauthorized', {
-        status: 401,
-      });
-    }
-
     const semester = request.nextUrl.searchParams.get('semester');
 
     if(!semester) throw new Error('semester is required');
