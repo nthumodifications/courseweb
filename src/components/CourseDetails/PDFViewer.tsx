1b6d7636d4551d20a9da6ade81393639c77cff30
ImJustChew <chewtzihwee@gmail.com>
2024-12-11 01:49:58 +0900

    feat: infer pdfjs-dist from packagejson

--- src/components/CourseDetails/PDFViewer.tsx
+++ src/components/CourseDetails/PDFViewer.tsx
@@ -4,10 +4,15 @@ import { Worker } from "@react-pdf-viewer/core";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
+import packageJson from "../../../package.json";
+
+const pdfjsVersion = packageJson.dependencies["pdfjs-dist"];
 
 const PDFViewer = ({ file }: { file: string }) => {
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+    <Worker
+      workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}
+    >
       <ScrollArea className="max-h-screen overflow-y-auto">
         <Viewer fileUrl={file} />
       </ScrollArea>
