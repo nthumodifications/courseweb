570d972a4dd09f2cecb4e36804e62f27ad36a5d4
ImJustChew <chewtzihwee@gmail.com>
2025-05-07 01:14:42 +0800

    feat: update VenueChip component; change button style from rounded-full to rounded-md

--- src/components/Timetable/VenueChip.tsx
+++ src/components/Timetable/VenueChip.tsx
@@ -8,7 +8,7 @@ export const VenueChip: FC<{
 }> = ({ venue, color, textColor }) => {
   return (
     <Link
-      className="flex flex-row justify-center items-center text-center text-[8px] md:text-[10px] rounded-full px-1 py-0.5"
+      className="flex flex-row justify-center items-center text-center text-[8px] md:text-[10px] rounded-md px-1 py-0.5"
       style={{ backgroundColor: color, color: textColor }}
       href={`/venues/${venue}`}
     >
